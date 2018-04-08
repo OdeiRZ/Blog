c88285299d0bf4f7628e3871b04d575540e23185
Odei Riveiro Zafra <odei.riveiro@gmail.com>
2018-04-09 01:03:23 +0200

    Agregado renderizado de directorios base

--- public/index.php
+++ public/index.php
@@ -5,10 +5,20 @@ ini_set('display_startups_erros', 1);
 error_reporting(E_ALL);
 
 include_once '../vendor/autoload.php';
-
 include_once '../config.php';
 
+$baseDir = str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
+$baseUrl = 'http://' . $_SERVER['HTTP_HOST'] . $baseDir;
+define('BASE_URL', $baseUrl);
+
 $route = isset($_GET['route']) ? $_GET['route'] : '/';
+
+function render($fileName, $params = []) {
+    ob_start();
+    extract($params);
+    include $fileName;
+    return ob_get_clean();
+}
 
 use Phroute\Phroute\RouteCollector;
 
@@ -17,7 +27,7 @@ $router->get('', function() use ($pdo) {
     $query = $pdo->prepare('SELECT * FROM blog_posts ORDER BY id DESC');
     $query->execute();
     $entradasBlog = $query->fetchAll(PDO::FETCH_ASSOC);
-    include '../views/index.php';
+    return render('../views/index.php', ['entradasBlog' => $entradasBlog]);
 });
 
 $dispatcher = new Phroute\Phroute\Dispatcher($router->getData());
