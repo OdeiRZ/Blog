1c77b4ae41d5c0914c884d718a8107fa58dce72b
Odei Riveiro Zafra <odei.riveiro@gmail.com>
2018-04-08 21:23:43 +0200

    Agregado esqueleto basico

---
+++ index.php
@@ -0,0 +1,55 @@
+<html>
+    <head>
+        <title>Blog</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <h1>Titulo</h1>
+            </div>
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="blog-post">
+                        <h2>Titulo Ejemplo</h2>
+                        <p>08/04/2018 por<a href="">Odei</a> </p>
+                        <div class="blog-post-image">
+                            <img src="imagenes/teclado.jpg" alt="Teclado">
+                        </div>
+                        <div class="blog-post-content">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        </div>
+                    </div>
+                    <div class="blog-post">
+                        <h2>Titulo Ejemplo</h2>
+                        <p>08/04/2018 por<a href="">Odei</a> </p>
+                        <div class="blog-post-image">
+                            <img src="imagenes/teclado.jpg" alt="Teclado">
+                        </div>
+                        <div class="blog-post-content">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        </div>
+                    </div>
+                    <div class="blog-post">
+                        <h2>Titulo Ejemplo</h2>
+                        <p>08/04/2018 por<a href="">Odei</a> </p>
+                        <div class="blog-post-image">
+                            <img src="imagenes/teclado.jpg" alt="Teclado">
+                        </div>
+                        <div class="blog-post-content">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </div>
+            </div>
+            <div class="row">
+                <footer>
+                    Pie de ejemplo
+                </footer>
+            </div>
+        </div>
+    </body>
+</html>
